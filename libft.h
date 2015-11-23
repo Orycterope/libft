8910c0c7e05ff3ed5773cd99b5bc8784b88a28e0
Thomas VERMEILH <tvermeil@e2r1p9.42.fr>
2015-11-23 15:54:15 +0100

    fonctions

--- libft.h
+++ libft.h
@@ -6,7 +6,7 @@
 /*   By: tvermeil <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2015/11/23 12:24:10 by tvermeil          #+#    #+#             */
-/*   Updated: 2015/11/23 13:26:39 by tvermeil         ###   ########.fr       */
+/*   Updated: 2015/11/23 15:23:51 by tvermeil         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,5 +19,6 @@ void	ft_putendl(char const *s);
 void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char const *s, int fd);
 void	ft_putendl_fd(char const *s, int fd);
+void	ft_putnbr(int n);
 
 #endif

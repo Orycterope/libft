359e129f77c232860b5e5a997b74dcf10af55e08
Thomas VERMEILH <tvermeil@e2r7p11.42.fr>
2015-11-23 13:34:08 +0100

    fist commit

---
+++ libft.h
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libft.h                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tvermeil <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/11/23 12:24:10 by tvermeil          #+#    #+#             */
+/*   Updated: 2015/11/23 13:26:39 by tvermeil         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFT_H
+# define LIBFT_H
+
+void	ft_putchar(char c);
+void	ft_putstr(char const *str);
+void	ft_putendl(char const *s);
+void	ft_putchar_fd(char c, int fd);
+void	ft_putstr_fd(char const *s, int fd);
+void	ft_putendl_fd(char const *s, int fd);
+
+#endif
